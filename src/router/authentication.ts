eb1a86fee5d868c5c3816fb4ea9bea70d4512617
chakripavankumar <siriyalapavankumar@gmail.com>
2024-01-03 13:29:53 +0530

    created 1st route✅

--- src/router/authentication.ts
+++ src/router/authentication.ts
@@ -1,3 +1,8 @@
-import expres from 'express';
+import express from 'express';
 
-import { register } from 'controllers/authentication';
+import { register } from '../controllers/authentication';
+import router from 'router';
+
+export default (router: express.Router ) => {
+    router.post('auth/register', register);
+};
